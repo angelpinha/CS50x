ead7246052f528023140dfdb1ef80a56a736e925
Angel Piña <inbox.angelpinha@gmail.com>
2023-01-26 00:45:16 -0400

    Basic web app template

---
+++ static/script.js
@@ -0,0 +1,16 @@
+// Theme switcher using a switch id="theme_switcher" with JS
+
+document.addEventListener("DOMContentLoaded", function() {
+  const themeSwitcher = document.getElementById("theme_switcher");
+  const theme = document.documentElement.getAttribute("data-theme");
+  
+  themeSwitcher.checked = theme === 'dark';
+  
+  themeSwitcher.addEventListener("change", () => {
+    if (themeSwitcher.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  });
+});
